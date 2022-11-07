dc5359efa2e253b060ae1f2ba70c55aaccf7a2e7
Nawzad75 <75nawsik@skola.boras.se>
2022-11-07 13:51:28 +0100

    Entity class

---
+++ ZoneRpg.Shared/Entity.cs
@@ -0,0 +1,26 @@
+namespace ZoneRpg
+{
+    class Entity
+    {
+        
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string CharSymbol { get; set; }
+
+        public int Hp { get; set; }
+
+        public Entity(int x, int y, string charSymbol, int hp)
+        {
+            X = x;
+            Y = y;
+            CharSymbol = charSymbol;
+            Hp = hp;
+        }
+
+
+
+
+    }
+
+
+}
