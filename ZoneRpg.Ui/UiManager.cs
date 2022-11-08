3e2cb1c2d0d9812afe00e85e321f9268e7f1fb6f
Nawzad75 <75nawsik@skola.boras.se>
2022-11-08 08:25:46 +0100

    Draw Zone1

--- ZoneRpg.Ui/UiManager.cs
+++ ZoneRpg.Ui/UiManager.cs
@@ -26,9 +26,17 @@ public class UiManager
 
         for (int i = 0; i < zone.Height+1; i++)
         {
-          Console.WriteLine("|                                            |");
+            //   Console.WriteLine("|                                            |");
+            Console.Write("|");
+            for (int j = 0; j < zone.Width-2; j++)
+            {
+                Console.Write(" ");
+
+            }
+            Console.WriteLine("|");
 
         }
+        
         for (int i = 0; i < zone.Width; i++)
         {
             Console.Write("-");
