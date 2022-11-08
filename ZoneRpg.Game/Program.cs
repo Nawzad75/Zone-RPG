c8a5c1af1df4df0ad5f1088804ffc5db83979346
Nawzad75 <75nawsik@skola.boras.se>
2022-11-08 08:03:06 +0100

    Zone class

--- ZoneRpg.Game/Program.cs
+++ ZoneRpg.Game/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ZoneRpg.Database;
+using ZoneRpg.Shared;
 using ZoneRpg.Ui;
 
 namespace ZoneRpg.Game
@@ -8,12 +9,12 @@ namespace ZoneRpg.Game
         private static void Main(string[] args)
         {
             DatabaseManager db = new DatabaseManager();
-            Class1 class1 = new Class1(db);
-
-
-
-
+            UiManager uiManager = new UiManager(db);
+            uiManager.Draw(new Zone(12,45,"Borås"));
+           
+           
 
         }
+        
     }
 }
---
+++ ZoneRpg.Ui/UiManager.cs
@@ -0,0 +1,40 @@
+﻿using ZoneRpg.Database;
+using ZoneRpg.Shared;
+
+namespace ZoneRpg.Ui;
+public class UiManager
+{
+    private DatabaseManager _db;
+
+    public UiManager(DatabaseManager db)
+    {
+        _db = db;
+    }
+    public void Draw(Zone zone)
+    {
+        // zone.Height=;
+        // zone.Width = 50;
+        // Console.WriteLine("Write Zone name:");
+        // zone.Name = Console.ReadLine();
+
+        for (int i = 0; i < zone.Width; i++)
+        {
+            Console.Write("-");
+
+        }
+        Console.Write("-");
+
+        for (int i = 0; i < zone.Height+1; i++)
+        {
+          Console.WriteLine("|                                            |");
+
+        }
+        for (int i = 0; i < zone.Width; i++)
+        {
+            Console.Write("-");
+
+        }
+
+
+    }
+}
